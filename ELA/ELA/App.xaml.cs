e14103bf32e2a124eedcecd8c9e1ca88fc8cbbe0
Emersonn <emersondinizz@yahoo.com.br>
2019-06-28 09:06:59 -0400

    ZERO

---
+++ ELA/ELA/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Threading.Tasks;
+using System.IO;
+using ELA.View;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace ELA
+{
+    public partial class App : Application
+    {
+        static DBase _dbase;
+        public static DBase DBase
+        {
+            get
+            {
+                if (_dbase == null)
+                {
+                    _dbase = new DBase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ELA.db3"));
+                }
+                return _dbase;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+            CarouselPage x = new CarouselPage();
+            x.Children.Add(new FalaLista());
+            x.Children.Add(new Fala());
+            MainPage = x;
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ ELA/ELA/DBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using System.Threading.Tasks;
+using ELA.Models;
+
+namespace ELA
+{
+    public class DBase
+    {
+        readonly SQLiteAsyncConnection _bancotexto;
+        public DBase(string Path)
+        {
+            _bancotexto = new SQLiteAsyncConnection(Path);
+            _bancotexto.CreateTableAsync<Models.Texto>().Wait();
+        }
+
+        public Task<List<Texto>> Listar()
+        {
+            return _bancotexto.Table<Texto>().ToListAsync();
+        }
+        public Task<Texto> Listar(int id)
+        {
+            return _bancotexto.Table<Texto>().Where(i => i.ID == id).FirstOrDefaultAsync();
+        }
+        public Task<int> Adicionar(Texto t)
+        {
+            return _bancotexto.InsertAsync(t);
+        }
+        public Task<int> Deletar(Texto t)
+        {
+            return _bancotexto.DeleteAsync(t);
+        }
+    }
+}
